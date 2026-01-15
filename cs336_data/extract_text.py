b6c5ad155922c34a8bd833b876a87521e20b4e49
Qianhe Xiao <lapis_xiao@outlook.com>
2026-01-15 17:00:55 +0800

    Add usage

--- cs336_data/extract_text.py
+++ cs336_data/extract_text.py
@@ -13,3 +13,18 @@ def extract_text(raw_html: bytes) -> str:
     html = raw_html.decode(encode_type, errors="replace")
     plain_text = extract_plain_text(html)
     return plain_text
+
+
+if __name__ == "__main__":
+    import sys
+    warc_path = sys.argv[1]
+    output_count = 10
+    with open(warc_path, "rb") as f:
+        for record in ArchiveIterator(f):
+            if record.record_type == WarcRecordType.response:
+                text = extract_text(record.reader.read())
+                print(f"--- Extracted text from {record.record_id} ---")
+                print(text)
+                output_count -= 1
+                if output_count == 0:
+                    break
