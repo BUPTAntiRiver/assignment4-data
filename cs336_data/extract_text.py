7d4c095a3e62c8cde7286536c69cdc7dc38a45e2
Qianhe Xiao <lapis_xiao@outlook.com>
2026-01-15 16:39:44 +0800

    Solve extract text

---
+++ cs336_data/extract_text.py
@@ -0,0 +1,15 @@
+"""
+Solution to problem extract_text
+"""
+
+
+from fastwarc.warc import ArchiveIterator, WarcRecordType
+from resiliparse.parse.encoding import detect_encoding
+from resiliparse.extract.html2text import extract_plain_text
+
+
+def extract_text(raw_html: bytes) -> str:
+    encode_type = detect_encoding(raw_html)
+    html = raw_html.decode(encode_type, errors="replace")
+    plain_text = extract_plain_text(html)
+    return plain_text
